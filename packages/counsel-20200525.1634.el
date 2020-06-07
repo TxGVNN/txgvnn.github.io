e84e9013efeefc61cebdca4aa1f9f27de7245859
Giap Tran <txgvnn@gmail.com>
2020-06-07 22:03:24 +0700

    package, Update version

--- packages/counsel-20200525.1634.el
+++ packages/counsel-20200525.1634.el
@@ -4,7 +4,7 @@
 
 ;; Author: Oleh Krehel <ohwoeowho@gmail.com>
 ;; URL: https://github.com/abo-abo/swiper
-;; Package-Version: 20200525.1633
+;; Package-Version: 20200525.1634
 ;; Version: 0.13.0
 ;; Package-Requires: ((emacs "24.5") (swiper "0.13.0"))
 ;; Keywords: convenience, matching, tools
